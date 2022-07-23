75b1cf25ccabfa70af8037f8d03740d8917a1611
Vallahor <vallahor91@gmail.com>
2022-07-23 15:44:12 -0300

    fix: icons working

--- backend/settings.py
+++ backend/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
+STATIC_ROOT = BASE_DIR.joinpath('static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
